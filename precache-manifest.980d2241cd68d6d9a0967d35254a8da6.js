fa23d582712e42ed502be8df735735f8d4d5503c
DevQNguyen <go.quang.nguyen@gmail.com>
2020-04-06 17:52:17 -0700

    Updates

---
+++ precache-manifest.980d2241cd68d6d9a0967d35254a8da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356971b47b1c05b00b8db5d8b33fefe2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0605b9fa0aa85e2bcdea",
+    "url": "/monsters-rolodex/static/css/main.53be876f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0605b9fa0aa85e2bcdea",
+    "url": "/monsters-rolodex/static/js/main.c5206b5f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
